feab3f57b3668b8af319481ef96fae188bca128d
Vitan <ivitan95@gmail.com>
2018-09-03 13:49:11 +0800

    New

--- Python/Zhuanhuan.py
+++ Python/Zhuanhuan.py
@@ -1,10 +1,10 @@
 # -*- coding: UTF-8 -*-
 # -*- auth: vitan -*-
-# 摄氏度转华氏度
+# 摄氏度转华氏度 华氏°F＝ ℃×9/5＋32
 clesius = float(input('请输入摄氏温度：'))
-fahrenheit = (clesius * 1.8 )+32
-print("转换后的温度是{:.1f}F".format(fahrenheit))
-# 华氏转度摄氏度
-fahrenheit = float(input('输入华氏温度:'))
-celsius = (fahrenheit - 32)/1.8
-print("转换后的温度是{:.1f}C".format(celsius))
+fahrenheit = clesius * 9/5 +32
+print("转换后的温度是{:.2f}°F".format(fahrenheit))
+# 华氏转度摄氏度 摄氏 ℃＝5/9（°F－32
+fahrenheit = float(input('请输入华氏温度:'))
+celsius = 5/9*(fahrenheit - 32)
+print("转换后的温度是{:.2f}°C".format(celsius))
