08f7844f5daa1c952ee543cda300484798816f38
Vitan <ivitan95@gmail.com>
2018-10-15 13:51:49 +0800

    修改：Python/Code/Cipin2/CipinInCh.py 新文件：Python/Code/Cipin2/mywor.png

--- Python/Code/Cipin2/CipinInCh.py
+++ Python/Code/Cipin2/CipinInCh.py
@@ -8,10 +8,10 @@ from  wordcloud import WordCloud
 with open('ch.txt','r',encoding = 'UTF-8') as f:
     content = f.read()
 
-# 分词
+# 结巴分词
 wordList = jieba.cut(content)
 
-#
+# 词频统计
 data = {}
 for word in wordList:
     if len(word)==1:
@@ -23,21 +23,22 @@ result = list(data.items())
 result.sort(key=lambda x:x[1],reverse=True) # 排序
 for i in range(20):
     print(result[i])
-resultList = list(result)
 
 # 设置词云
+wordSplit = " ".join(dict(result))
 wc = WordCloud(background_color="black",  # 设置背景颜色
                # mask = "图片",  #设置背景图片
                max_words=2000,  # 设置最大显示的字数
                # stopwords = "", #设置停用词
-               font_path="fangsong_GB2312.ttf",
+               font_path="/usr/share/fonts/noto-cjk/NotoSansCJK-Medium.ttc",
                # 设置中文字体，使得词云可以显示（词云默认字体是“DroidSansMono.ttf字体库”，不支持中文）
                max_font_size=50,  # 设置字体最大值
                random_state=30,  # 设置有多少种随机生成状态，即有多少种配色方案
                )
-mywc = wc.generate(result)  # 生成词云
+mywc = wc.generate(wordSplit)  # 生成词云
 
 # 展示词云图
 plt.imshow(mywc)
 plt.axis("off")
 plt.show()
+wc.to_file('mywor.png')  # 保存图片文件
