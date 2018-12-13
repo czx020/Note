0b17e543744cb2bcc75f63631e77d97d701926b5
Vitan <ivitan95@gmail.com>
2018-12-13 19:16:28 +0800

    Update PolynomialFeatures.py

--- Python/Code/Sklearn/LinearRegression/PolynomialFeatures.py
+++ Python/Code/Sklearn/LinearRegression/PolynomialFeatures.py
@@ -1,32 +1,51 @@
-import numpy as np
-from sklearn.datasets import load_boston
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import PolynomialFeatures
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 18-12-13 下午6:18
+# @Author  : Vitan
+# @File    : PolynomialBoston.py
 
+from sklearn.datasets import load_boston
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import PolynomialFeatures
+import matplotlib.pyplot as plt
+
+# 读取数据集
 boston = load_boston()
 
-x = boston.data[:,12].reshape(-1,1)
+print(boston.keys())
+print(boston.target)# 房价数据
+print(boston.feature_names) # 数据集特征
+
+# 划分训练集与测试集
+#随机擦痒25%的数据构建测试样本，剩余作为训练样本
+X_train,X_test,y_train,y_test = train_test_split(boston.data,boston.target,test_size=0.3) #random_state：是随机数的种子
+print(X_train.shape,y_train.shape)
+
+# 建立模型
+X = X_train[:,12].reshape(-1,1)
 y = boston.target
 
-LineR = LinearRegression()
-LineR.fit(x,y)
-w = LineR.coef_
-b = LineR.intercept_
-plt.scatter(x,y)
-plt.plot(x,w*x+b,'r')
-plt.show()
+LineR2 = LinearRegression()
+LineR2.fit(X,y)
+y_predict = LineR2.predict(X)
 
-p = PolynomialFeatures(degree=2)
-x_poly = p.fit_transform(x)
-lp = LinearRegression()
-lp.fit(x_poly,y)
-y_poly = lp.predict(x_poly)
+poly = PolynomialFeatures(degree=2)
+X_ploy = poly.fit_transform(X)
 
 lrp = LinearRegression()
-lrp.fit(x_poly,y)
-plt.scatter(x,y)
-plt.scatter(x,w*x+b,'r')
-plt.scatter(x,y_poly)
+lrp.fit(X_ploy,y)
+y_predict_p = lrp.predict(X_ploy)
+
+plt.scatter(X,y)
+plt.plot(X,y_predict_p,'r')
+plt.show()
+
+poly = PolynomialFeatures(degree=2)
+X_ploy = poly.fit_transform(X)
+
+lrp = LinearRegression()
+lrp.fit(X_ploy,y)
+plt.scatter(X,y_predict)
+plt.scatter(X,y_predict_p)
 plt.show()
